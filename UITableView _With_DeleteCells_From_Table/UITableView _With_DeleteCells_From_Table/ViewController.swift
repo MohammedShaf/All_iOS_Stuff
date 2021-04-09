6337e212a86f77269f7b793d5ee1058d72185cde
Shafiullah, Mohammed (Cognizant) <Mohammed.Shafiullah@cognizant.com>
2021-04-09 16:21:26 +0530

    Pushing the code into UItableView_with_DeleteCell_From_Table_Branch

---
+++ UITableView _With_DeleteCells_From_Table/UITableView _With_DeleteCells_From_Table/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableView _With_DeleteCells_From_Table
+//
+//  Created by Shafiullah, Mohammed (Cognizant) on .
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
